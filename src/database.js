ad139c76431d0a0603d45f05bc51b6f978479852
Raul <iraulcv@gmail.com>
2022-04-13 02:37:36 -0500

    updating database conexcion and dependencies

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/notesdb', {
+console.log(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGO_DB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
